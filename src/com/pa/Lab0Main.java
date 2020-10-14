891c24a70b57653046649c76c5a1297f62d364ad
Diogo-Felix <diogofelix092@gmail.com>
2020-10-14 14:18:48 +0100

    Initial commit

---
+++ src/com/pa/Lab0Main.java
@@ -0,0 +1,18 @@
+package com.pa;
+
+import com.pa.Person;
+
+public class Lab0Main {
+
+    public static void main(String[] args){
+        Person p1 = new Person(1, "Manuel");
+        Person p2 = new Person(2,"Diogo");
+        Person p3 = new Person(3, "António");
+
+        Group group = new Group(p1);
+        group.addMember(p2);
+        group.addMember(p3);
+
+        System.out.println(group.toString());
+    }
+}
